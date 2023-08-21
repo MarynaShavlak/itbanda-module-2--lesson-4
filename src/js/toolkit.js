260777f7870c6a3423790fa5a31de09c17a426cc
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-08-21 23:36:09 +0300

    add logic for rooms slider

--- src/js/toolkit.js
+++ src/js/toolkit.js
@@ -1,4 +1,9 @@
 const toolkitsList = document.querySelectorAll('.plus-btn--toolkit');
+const roomsBtnList = document.querySelectorAll('.rooms__item');
+const kitchenSchema = document.querySelector('.schema--kitchen');
+const roomSchema = document.querySelector('.schema--room');
+const bathSchema = document.querySelector('.schema--bath');
+
 toolkitsList.forEach(el => {
   el.addEventListener('mouseenter', () => {
     toggleToolkitDescVisibility(el);
@@ -8,8 +13,34 @@ toolkitsList.forEach(el => {
   });
 });
 
+roomsBtnList.forEach(el => {
+  el.addEventListener('click', e => {
+    onRoomBtnClick(e);
+  });
+});
+
 function toggleToolkitDescVisibility(el) {
   const parentItem = el.closest('.toolkit__wrapper');
   const descItem = parentItem.querySelector(':first-child');
   descItem.classList.toggle('is-shown');
+}
+
+function onRoomBtnClick(e) {
+  const clickedButton = e.target;
+  if (clickedButton.classList.contains('rooms__item--current')) return;
+  roomsBtnList.forEach(button => {
+    if (button === clickedButton) {
+      const id = button.id;
+      setRoomSchemaToDisplay(id);
+      button.classList.add('rooms__item--current');
+    } else {
+      button.classList.remove('rooms__item--current');
+    }
+  });
+}
+
+function setRoomSchemaToDisplay(id) {
+  kitchenSchema.style.display = id === 'room-1' ? 'block' : 'none';
+  roomSchema.style.display = id === 'room-2' ? 'block' : 'none';
+  bathSchema.style.display = id === 'room-3' ? 'block' : 'none';
 }
