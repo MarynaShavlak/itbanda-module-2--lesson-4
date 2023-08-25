c95bbf2857c96cce9b86e3487e830b6b83446707
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-08-25 15:50:05 +0300

    add modal open logic

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,30 @@
-const refs = {
+const refsSupport = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
+  backdrop: document.querySelector('.backdrop--support'),
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
+refsSupport.openModalBtn.addEventListener('click', toggleSupportModal);
+refsSupport.closeModalBtn.addEventListener('click', toggleSupportModal);
+refsSupport.backdrop.addEventListener('click', toggleSupportModal);
 
-function toggleModal() {
+function toggleSupportModal() {
   document.body.classList.toggle('modal-open');
-  refs.modal.classList.toggle('backdrop--hidden');
+  refsSupport.modal.classList.toggle('backdrop--hidden');
+}
+const refsSubscr = {
+  openModalBtn: document.querySelector('[data-subscription-modal-open]'),
+  closeModalBtn: document.querySelector('[data-subscription-modal-close]'),
+  modal: document.querySelector('[data-subscription-modal]'),
+  backdrop: document.querySelector('.backdrop--subscr'),
+};
+
+refsSubscr.openModalBtn.addEventListener('click', toggleSubscriptionModal);
+refsSubscr.closeModalBtn.addEventListener('click', toggleSubscriptionModal);
+refsSubscr.backdrop.addEventListener('click', toggleSubscriptionModal);
+
+function toggleSubscriptionModal() {
+  document.body.classList.toggle('modal-open');
+  refsSubscr.modal.classList.toggle('backdrop--hidden');
 }
