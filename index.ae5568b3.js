289e373f829952727c25ed4331d7c1159900a8a3
MarynaShavlak <111526360+MarynaShavlak@users.noreply.github.com>
2023-08-25 12:52:04 +0000

    Deploying to gh-pages from @ MarynaShavlak/cleaning@c95bbf2857c96cce9b86e3487e830b6b83446707 🚀

--- index.ae5568b3.js
+++
@@ -1,2 +0,0 @@
-const o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){document.body.classList.toggle("modal-open"),o.modal.classList.toggle("backdrop--hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e);
-//# sourceMappingURL=index.ae5568b3.js.map
