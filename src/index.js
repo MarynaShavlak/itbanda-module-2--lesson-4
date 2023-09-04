9f8cf3afca1bbe440dfd0f29564ada2decbcab97
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-09-04 23:47:32 +0300

    fix redirect code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const currentPage = window.location.pathname;
   if (currentPage === '/' || currentPage === '/index.html') {
-    document.querySelector('.nav__link ').classList.add('nav__link--current');
+    document.querySelector('.nav ').classList.add('nav__link--current');
   }
 });
