03ee1809f59c1ae072a2edc75778a741e408c676
MarynaShavlak <111526360+MarynaShavlak@users.noreply.github.com>
2023-09-04 20:49:58 +0000

    Deploying to gh-pages from @ MarynaShavlak/cleaning@9f8cf3afca1bbe440dfd0f29564ada2decbcab97 🚀

---
+++ index.db98508b.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var n=window.location.pathname;"/"!==n&&"/index.html"!==n||document.querySelector(".nav ").classList.add("nav__link--current")}));
+//# sourceMappingURL=index.db98508b.js.map
