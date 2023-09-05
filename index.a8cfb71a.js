11a9a53f555eebdf2f8a702d152b61ebf9b2e9b9
MarynaShavlak <111526360+MarynaShavlak@users.noreply.github.com>
2023-09-05 21:00:13 +0000

    Deploying to gh-pages from @ MarynaShavlak/cleaning@6abc127358ef0c888c7dc4f70c211f39dcc3a69f 🚀

--- index.a8cfb71a.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(function(){const n=window.location.pathname;"/"!==n&&"/index.html"!==n||document.querySelector(".nav ").classList.add("nav__link--current")}));
-//# sourceMappingURL=index.a8cfb71a.js.map
