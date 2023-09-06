ee9ea9c786ddd9b65d193dbe672d748458799b20
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-09-06 17:55:31 +0300

    add hero code for office page

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const currentPage = window.location.pathname;
   if (currentPage === '/' || currentPage === '/index.html') {
     document.querySelector('.nav__link ').classList.add('nav__link--current');
+  } else if (currentPage === '/office.html') {
+    document
+      .querySelector('.nav__list .nav__item:nth-child(2) .nav__link')
+      .classList.add('nav__link--current');
   }
 });
