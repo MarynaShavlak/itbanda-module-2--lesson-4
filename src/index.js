0e60aae4f152b6bdc63a10bdb2d1ab1b02a4fefc
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-09-07 18:54:22 +0300

    add block with cost calc

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   const currentPage = window.location.pathname;
+  console.log('currentPage: ', currentPage);
+  const buttonToHide = document.querySelector('.modal-btn');
+  console.log('buttonToHide: ', buttonToHide);
   if (currentPage === '/' || currentPage === '/index.html') {
     document.querySelector('.nav__link ').classList.add('nav__link--current');
   } else if (currentPage === '/office.html') {
     document
       .querySelector('.nav__list .nav__item:nth-child(2) .nav__link')
       .classList.add('nav__link--current');
+    buttonToHide.style.display = 'none';
   }
 });
