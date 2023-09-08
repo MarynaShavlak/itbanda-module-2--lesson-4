e58c467816707958a6a33daef117db34e3eae47e
MarynaShavlak <111526360+MarynaShavlak@users.noreply.github.com>
2023-09-08 12:30:08 +0000

    Deploying to gh-pages from @ MarynaShavlak/cleaning@28e82def12206169ea78b0f25f961ea7e84667d6 🚀

---
+++ office.fb13e05f.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".buildings__element");e.forEach((n=>{n.addEventListener("click",(n=>{!function(n){const t=n.target;t.classList.contains("buildings__element--current")||e.forEach((e=>{e===t?(e.id,e.classList.add("buildings__element--current")):e.classList.remove("buildings__element--current")}))}(n)}))}));
+//# sourceMappingURL=office.fb13e05f.js.map
