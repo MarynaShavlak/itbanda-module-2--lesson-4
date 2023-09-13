9e09cefdadd64bb9e652e10be12904cb99c7f257
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-09-14 00:43:15 +0300

    logic for updating services quantity

--- src/js/office-order-calc.js
+++ src/js/office-order-calc.js
@@ -5,6 +5,7 @@ const decreaseSquareBtn = document.querySelector(
 );
 
 const squareValueElementsList = document.querySelectorAll('.square-value');
+const servicesElementsList = document.querySelectorAll('.service-element');
 const serviceCheckboxList = document.querySelectorAll(
   '.service-element .checkbox'
 );
@@ -24,7 +25,18 @@ serviceCheckboxList.forEach(el => {
   });
 });
 
-let currentSquareValue = 1;
+const itemQuantities = {
+  square: 1,
+  windowsStandard: 1,
+  windowsLarge: 1,
+  microWave: 1,
+  refrigerator: 1,
+  plate: 1,
+  officeChair: 1,
+  sofaDry2x: 1,
+  sofaDry3x: 1,
+  sofaDry4x: 1,
+};
 
 function onBuldingTypeBtnClick(e) {
   const clickedButton = e.target;
@@ -41,32 +53,72 @@ function onBuldingTypeBtnClick(e) {
 
 function updateSquareInterfaceValue() {
   squareValueElementsList.forEach(el => {
-    el.textContent = currentSquareValue;
+    el.textContent = itemQuantities.square;
   });
 }
 
+function updateServiceItemInterface(serviceName) {
+  const label = [...servicesElementsList].find(
+    el => el.getAttribute('data-id') === serviceName
+  );
+  const itemQntEl = label.nextElementSibling.querySelector(
+    `[data-name="${serviceName}"]`
+  );
+  itemQntEl.textContent = itemQuantities[serviceName];
+}
+
 function onChangeSquareBtnClick(e) {
-  const clickedBtn = e.currentTarget.className.split(' ')[1];
-  changeSquareValue(clickedBtn);
+  const operationType = getClickedBtnType(e);
+  changeSquareValue(operationType);
   updateSquareInterfaceValue();
   updateSquareTotalCost();
   updateMinusBtnStyle();
 }
 
-function changeSquareValue(clickedBtn) {
-  if (clickedBtn === 'control-quantity-btn--plus') {
-    currentSquareValue += 1;
-  } else if (clickedBtn === 'control-quantity-btn--minus') {
-    if (currentSquareValue === 1) return;
-    currentSquareValue -= 1;
+function changeSquareValue(operation) {
+  if (operation === 'plus') {
+    itemQuantities.square += 1;
+  } else if (operation === 'minus') {
+    if (itemQuantities.square === 1) return;
+    itemQuantities.square -= 1;
   }
+}
+
+function changeServiceItemValue(itemName, operation) {
+  if (operation === 'plus') {
+    itemQuantities[itemName] += 1;
+  } else if (operation === 'minus') {
+    if (itemQuantities[itemName] === 1) return;
+    itemQuantities[itemName] -= 1;
+  }
+}
+
+function getClickedBtnType(e) {
+  const type = e.currentTarget.getAttribute('data-type');
+  return type;
+}
+
+function getChosenServiceName(e) {
+  const chosenServiceName = e.currentTarget
+    .closest('.wrap--service')
+    .parentNode.querySelector('label')
+    .getAttribute('data-id');
+  return chosenServiceName;
+}
+
+function onChangeQuantityBtnClick(e) {
+  const serviceName = getChosenServiceName(e);
+  const operationType = getClickedBtnType(e);
+  changeServiceItemValue(serviceName, operationType);
+  updateServiceItemInterface(serviceName);
+  // updateMinusBtnStyle();
 }
 
 function updateMinusBtnStyle() {
   const decreaseSquareIcon = document.querySelector(
     '.control-quantity-btn--minus .icon--minus'
   );
-  if (currentSquareValue === 1) {
+  if (square === 1) {
     decreaseSquareIcon.style.fill = 'rgba(	77, 18, 153, 0.3)';
   } else {
     decreaseSquareIcon.style.fill = '#4D1299';
@@ -75,7 +127,7 @@ function updateMinusBtnStyle() {
 
 function updateSquareTotalCost() {
   const totalSquareCostEl = document.querySelector('#squareValue');
-  totalSquareCostEl.textContent = `${2 * currentSquareValue}`;
+  totalSquareCostEl.textContent = `${2 * itemQuantities.square}`;
 }
 
 function toggleServiceItem(e) {
@@ -83,13 +135,22 @@ function toggleServiceItem(e) {
   const label = el.closest('label');
   const controlQuantityBlock = label.nextElementSibling;
   controlQuantityBlock.classList.toggle(`isHidden`, !el.checked);
+  const increaseQuantityBtn = controlQuantityBlock.querySelector(
+    '.control-quantity-btn--plus'
+  );
+  const decreaseQuantityBtn = controlQuantityBlock.querySelector(
+    '.control-quantity-btn--minus'
+  );
+
+  increaseQuantityBtn.addEventListener('click', onChangeQuantityBtnClick);
+  decreaseQuantityBtn.addEventListener('click', onChangeQuantityBtnClick);
+
   const serviceItem = createServiceItem(label);
   const isServiceChosen = el.checked;
   updateTotalCostTable(isServiceChosen, serviceItem);
 }
 
 function createServiceItem(element) {
-  console.log('element: ', element);
   const serviceName = element.querySelector(
     '.service-element__text'
   ).textContent;
@@ -102,10 +163,10 @@ function createServiceItem(element) {
   const nameSpan = document.createElement('span');
   nameSpan.className = 'item__name';
   nameSpan.textContent = `${serviceName}`;
-  const spaceTextNode = document.createTextNode('\u00A0');
+  const spaceTextNode = document.createTextNode('\u00A0 x');
   const quantitySpan = document.createElement('span');
-  quantitySpan.className = 'item__quantity';
-  quantitySpan.textContent = 'x1';
+  quantitySpan.className = 'item__quantity service-quantity';
+  quantitySpan.textContent = '1';
   nameSpan.appendChild(spaceTextNode);
   nameSpan.appendChild(quantitySpan);
   const valueSpan = document.createElement('span');
@@ -122,10 +183,10 @@ function createServiceItem(element) {
 function updateTotalCostTable(isServiceChosen, item) {
   const itemId = item.id;
   const tableElementsList = totalCostTableElement.querySelectorAll('li');
-  console.log('tableElementsList: ', tableElementsList);
   const chosenItem = [...tableElementsList].find(el => el.id === itemId);
-  console.log('chosenItem : ', chosenItem);
   if (isServiceChosen && !chosenItem) {
     totalCostTableElement.insertAdjacentElement('beforeend', item);
+  } else if (!isServiceChosen && chosenItem) {
+    totalCostTableElement.removeChild(chosenItem);
   }
 }
