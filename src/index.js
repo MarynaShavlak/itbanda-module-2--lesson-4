e7d80101eb1202c7c3ffdd63c6fd42d01acfc73f
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-09-19 19:06:26 +0300

    add logic for selecting hour and minutes

--- src/index.js
+++ src/index.js
@@ -787,3 +787,63 @@ const clockIcon = document.querySelector('.icon--clock');
 // clockIcon.addEventListener('click', () => {
 //   timePickerElement.focus();
 // });
+
+const hourTablo = document.querySelector('.tablo--hours');
+const minuteTablo = document.querySelector('.tablo--minutes');
+const hourPicker = document.querySelector('.time-picker__hours');
+const minutePicker = document.querySelector('.time-picker__minutes');
+
+hourTablo.addEventListener('click', e =>
+  onTimeCellClick(e, '.time-picker__hours')
+);
+minuteTablo.addEventListener('click', e =>
+  onTimeCellClick(e, '.time-picker__minutes')
+);
+minutePicker.addEventListener('click', () =>
+  toggleTablo(minuteTablo, hourTablo)
+);
+hourPicker.addEventListener('click', () => toggleTablo(hourTablo, minuteTablo));
+
+function onTimeCellClick(e, blockSelector) {
+  const clickedElement = e.target;
+  if (!validateClickedNumber(clickedElement)) return;
+  const elements = document.querySelectorAll('.number');
+  updateTimePickerTablo(clickedElement, elements);
+  const block = document.querySelector(blockSelector);
+  const value = clickedElement.dataset.id;
+  updateTimePickerBlock(block, value);
+}
+
+function validateClickedNumber(clickedElement) {
+  return (
+    clickedElement.classList.contains('number') &&
+    !clickedElement.classList.contains('active')
+  );
+}
+
+function updateTimePickerTablo(clickedElement, elements) {
+  [...elements].map(element =>
+    element === clickedElement
+      ? selectElement(element)
+      : deselectElement(element)
+  );
+}
+
+function selectElement(element) {
+  element.classList.add('active');
+}
+
+function deselectElement(element) {
+  element.classList.remove('active');
+}
+
+function updateTimePickerBlock(block, value) {
+  block.innerHTML = value;
+}
+
+function toggleTablo(tabloToShow, tabloToHide) {
+  const isVisible = !tabloToShow.classList.contains('isHidden');
+  if (isVisible) return;
+  tabloToShow.classList.remove('isHidden');
+  tabloToHide.classList.add('isHidden');
+}
