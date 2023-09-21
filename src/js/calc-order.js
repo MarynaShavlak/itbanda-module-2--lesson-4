8076e4675ae22188745185acc04fc6524c11ddea
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-09-21 21:19:53 +0300

    add scripts for all html pages

--- src/js/calc-order.js
+++ src/js/calc-order.js
@@ -1,2 +1,8 @@
 import './modules/common.js';
 import './modules/aside-menu.js';
+import './modules/calendar.js';
+import './modules/time-picker.js';
+import './modules/location.js';
+import './modules/chose-services.js';
+import './modules/modal.js';
+import './modules/subscr-modal.js';
